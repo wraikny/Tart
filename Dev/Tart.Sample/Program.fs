f5eddcf096a22ead6dda37caaff33c029a8ffe72
wraikny <writtenknown@gmail.com>
2019-04-09 21:44:49 +0900

    let code look nice

--- Dev/Tart.Sample/Program.fs
+++ Dev/Tart.Sample/Program.fs
@@ -8,13 +8,12 @@ module Program =
     let readLine () = Console.ReadLine()
 
     let readLineInt() =
-        let success, result =
-            readLine () |> Int32.TryParse
+        let success, result = readLine () |> Int32.TryParse
         if success then Some result else None
 
 
     [<EntryPoint>]
-    let main argv = 
+    let main _ = 
         let messenger = Counter.messengerBuilder()
 
         let rec loop view =
@@ -22,15 +21,17 @@ module Program =
 
             printfn "q: Quit, a: Add, s: Sub, c:Clear, r:Random"
             printf "Input Messege:"
-            let continueFlag =
+
+            let continue' =
                 Console.ReadLine() |> function
                 | "q" -> false
+
                 | "a" | "s" as s ->
                     printf "Input Number:"
-                    readLineInt() |> function
+                    readLineInt () |> function
                     | None ->
                         printfn "Input correctly!"
-                        ()
+
                     | Some n ->
                         n
                         |> if s = "a" then Counter.Msg.Add else Counter.Msg.Sub
@@ -41,28 +42,29 @@ module Program =
                 | "c" ->
                     messenger.PushMsg(Counter.Msg.Clear)
                     true
+
                 | "r" ->
                     printf "Input Max of Random Number:"
-                    readLineInt() |> function
+                    readLineInt () |> function
                     | None ->
                         printfn "Input correctly!"
-                        ()
+                        
                     | Some n ->
                         messenger.PushMsg(Counter.Msg.Random(0, n))
                     true
+
                 | _ ->
                     printfn "Input correctly!"
                     true
 
-            if continueFlag then
+            if continue' then
                 Thread.Sleep(10)
+
                 messenger.TryViewModel |> function
                 | Some view -> loop view
                 | None ->
                     printfn "Failed to get viewModel"
                     loop view
-            else
-                ()
 
 
         messenger.StartAsync() |> ignore
