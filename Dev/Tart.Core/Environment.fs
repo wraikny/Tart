6636150690920950c355414d8a407a5cc1610c5d
wraikny <writtenknown@gmail.com>
2019-04-16 21:01:07 +0900

    improved Env

--- Dev/Tart.Core/Environment.fs
+++ Dev/Tart.Core/Environment.fs
@@ -7,11 +7,24 @@ type internal IEnvCore =
     abstract Random : Random
 
 [<Class>]
-type Environment<'ViewMsg>() =
+type public Environment<'ViewMsg>() =
+    
+    let mutable random : Random = new Random()
+    let mutable updater : IMsgSender<'ViewMsg> option = None
 
-    member val internal Updater : IMsgSender<'ViewMsg> option = None with get, set
+    member __.Updater
+        with get() = updater
+
+    member this.SetUpdater(updater' : #IMsgSender<_>) =
+        updater <- Some(updater' :> IMsgSender<_>)
+        this
+
+    member this.SetRandom(random' : Random) =
+        random <- random'
+        this
 
     static member Initialize<'ViewMsg>() = new Environment<'ViewMsg>()
 
     interface IEnvCore with
-        member val Random = new Random()
+        member this.Random
+            with get() = random
