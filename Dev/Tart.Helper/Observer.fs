11938f79b0aee49fd1827cb77f3b310b429907e5
wraikny <writtenknown@gmail.com>
2019-04-20 19:03:24 +0900

    Observer

---
+++ Dev/Tart.Helper/Observer.fs
@@ -0,0 +1,20 @@
+﻿namespace wraikny.Tart.Helper
+
+[<Interface>]
+type IObserver<'T> =
+    abstract UpdateFromNotify : 'T -> unit
+
+
+open System.Collections.Generic
+
+
+[<AbstractClass>]
+type Observable<'T>() =
+    let observers = new List<IObserver<'T>>()
+
+    member this.AddObserver(observer) =
+        observers.Add(observer)
+
+
+    member this.NotifyObservers(input) =
+        observers.ForEach(fun o -> o.UpdateFromNotify(input))
