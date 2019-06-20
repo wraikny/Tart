6e2a03ab95f31157de7e3fb14a8db5add663393d
wraikny <writtenknown@gmail.com>
2019-06-20 19:00:18 +0900

    implemented binary search

---
+++ Dev/Tart.Helper/Math/Utils.fs
@@ -0,0 +1,33 @@
+﻿namespace wraikny.Tart.Helper.Math.Utils
+
+open wraikny.Tart.Helper.Math
+
+module BinarySearch =
+    let inline binarySearch zero two count predicate current target =
+        let rec search count diffSum current target =
+            if count <= 0 then diffSum
+            else
+                let middle = (current + target) / two
+                let newDiffSum = diffSum + (middle - current)
+
+                if predicate newDiffSum then
+                    search (count - 1) newDiffSum middle target
+                else
+                    search (count - 1) diffSum current middle
+
+        search count zero current target
+
+
+    let inline generic count predicate current target =
+        let zero = LanguagePrimitives.GenericZero
+        let one = LanguagePrimitives.GenericOne
+        let two = one + one
+        binarySearch zero two
+            count predicate current target
+
+
+    let inline vector count predicate current target =
+        let one = Vector.one()
+        let two = one + one
+        binarySearch (Vector.zero()) two
+            count predicate current target
