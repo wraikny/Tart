5667e8540d97c6ffa0239ef93fbd0cb0ad2eac59
wraikny <writtenknown@gmail.com>
2019-07-01 23:02:41 +0900

    Crypt access modifier

--- Dev/Tart.Sockets/Crypt.fs
+++ Dev/Tart.Sockets/Crypt.fs
@@ -4,14 +4,14 @@ open System.Security.Cryptography
 
 // https://qiita.com/ak2ie/items/f97ee5265527507f5308
 
-let encryptor (crypto : ICryptoTransform) encoder msg =
+let internal encryptor (crypto : ICryptoTransform) encoder msg =
     let bytes = encoder msg
 
     let encryptedBytes = crypto.TransformFinalBlock(bytes, 0, bytes.Length)
 
     encryptedBytes
 
-let decryptor (crypto : ICryptoTransform) decoder bytes =
+let internal decryptor (crypto : ICryptoTransform) decoder bytes =
     let decryptedBytes = crypto.TransformFinalBlock(bytes, 0, bytes.Length)
 
     decoder decryptedBytes
