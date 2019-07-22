62ea10307e43e1b3769441a0bb218a6eb7f799b9
wraikny <writtenknown@gmail.com>
2019-07-23 06:00:28 +0900

    refine

--- Dev/Tart.Helper/Math/Dependent.fs
+++ Dev/Tart.Helper/Math/Dependent.fs
@@ -5,15 +5,32 @@ open FSharpPlus
 
 // https://7colou.red/blog/2018/03-08-fsharp-type-level-computation/index.html
 
-type Ty<'Type> = struct end
+[<Struct>]
+type Ty<'Type> = Ty of 'Type with
+    member x.Value = x |> function Ty v -> v
 
-let inline ty< ^Type > : Ty< ^Type > = Ty()
+let inline ty< ^Type > : Ty< ^Type > = Ty Unchecked.defaultof< ^Type >
 
 let inline eval (x: Ty< ^A >) : Ty< ^B > =
     (^A: (static member inline eval: Ty< ^A > -> Ty< ^B >) x)
 
+
+type Add<'x, 'y> = Add of 'x * 'y with
+    static member inline eval (_: Ty<Add< ^X, ^Y>>) : _
+        when ^X: (static member eval: Ty< ^X > -> Ty< ^X' >) =
+        (^X': (static member add: _*_ -> _) ty< ^X' >,ty< ^Y >)
+
+
+type Sub<'x, 'y> = Sub of 'x * 'y with
+    static member inline eval (_: Ty<Sub< ^X, ^Y>>) : _
+        when ^X: (static member eval: Ty< ^X > -> Ty< ^X' >)
+        and ^Y: (static member eval: Ty< ^Y > -> Ty< ^Y' >) =
+        (^Y': (static member sub: _*_ -> _) ty< ^X' >,ty< ^Y' >)
+
+
 [<Struct>]
-type Peano = private | Peano
+type Peano = Peano
+
 
 [<Struct>]
 type Z = Z with
@@ -23,7 +40,12 @@ type Z = Z with
 
     static member eval (x: Ty<Z>) = x
 
+    static member inline add (_: Ty<Z>, y) = eval y
+
     static member inline sub (x, _: Ty<Z>) = eval x
+
+    static member inline mul (_: Ty<Z>, _) = ty<Z>
+    static member inline mul (_, _: Ty<Z>) = ty<Z>
 
 
 type S< ^n > = S of ^n with
@@ -40,28 +62,29 @@ type S< ^n > = S of ^n with
     static member inline eval (_: Ty<S< ^N >>) : _
         when ^N: (static member eval: Ty< ^N > -> Ty< ^N' >) = ty<S< ^N' >>
 
+    static member inline add (_: Ty<S< ^X >>, _: Ty< ^Y >) : _
+        when ^X: (static member eval: Ty< ^X > -> Ty< ^X' >)
+        and ^X': (static member add: Ty< ^X' > * Ty< ^Y > -> Ty< ^Z >) =
+        ty<S< ^Z >>
+
     static member inline sub (_: Ty<S< ^X> >, _: Ty<S< ^Y >>) : _
         when ^X: (static member eval: Ty< ^X > -> Ty< ^X' >)
         and ^Y: (static member eval: Ty< ^Y > -> Ty< ^Y' >)
         and ^Y': (static member sub: Ty< ^X' > * Ty< ^Y' > -> Ty< ^Z >) =
         ty< ^Z >
 
-type Sub<'x, 'y> = Sub of 'x * 'y with
-    static member inline eval (_: Ty<Sub< ^X, ^Y>>) : _
+    static member inline mul (_: Ty<S< ^X >>, _: Ty<S< ^Y >>) : _
         when ^X: (static member eval: Ty< ^X > -> Ty< ^X' >)
-        and ^Y: (static member eval: Ty< ^Y > -> Ty< ^Y' >) =
-        (^Y': (static member sub: _*_ -> _) ty< ^X' >,ty< ^Y' >)
+        and ^Y: (static member eval: Ty< ^Y > -> Ty< ^Y' >)
+        and ^X': (static member mul: Ty< ^X' > * Ty< ^Y' > -> Ty< ^Z >) =
+        ty< ^Z >
 
-type ^N GE1 = ^N S
-type ^N GE2 = ^N GE1 S
-type ^N GE3 = ^N GE2 S
-type ^N GE4 = ^N GE3 S
 
 type N0 = Z
-type N1 = Z GE1
-type N2 = Z GE2
-type N3 = Z GE3
-type N4 = Z GE4
+type N1 = N0 S
+type N2 = N1 S
+type N3 = N2 S
+type N4 = N3 S
 
 
 module Peano =
@@ -82,34 +105,40 @@ type Vector<'a, ^n
     when ^n : (static member PeanoImpl : ^n -> Peano)
     and  ^n : (static member toInt : ^n -> int)
     and  ^n : (static member eval : Ty< ^n > -> Ty< ^n >)
-    > = private Vec of 'a [] * ^n with
+    > = Vec of 'a [] * ^n with
 
-    member inline private x.n = x |> function | Vec(_, n) -> n
-    member inline private x.array = x |> function | Vec(a, _) -> a
+    member inline x.Peano = x |> function | Vec(_, n) -> n
+    member inline x.Array = x |> function | Vec(a, _) -> a
 
-    static member inline ToSeq (v : Vector<'t, 'N>) : seq<'t> =
-        Array.toSeq v.array
+    member inline v.Item (i : ^i) : 'a =
+        eval ty<Sub<Sub< ^n, ^i >, Z S>> |> ignore
+        let index = (^i : (static member toInt : ^i -> int) i)
+        v.Array.[index]
+
+    static member inline ToSeq (v : Vector<'t, _>) : seq<'t> =
+        Array.toSeq v.Array
 
     member inline v.MapArray(f : 'a [] -> 't []) : Vector<'t, 'n> =
         v |> function Vec(arr, n) -> Vec(f arr, n)
 
-    static member inline Map(v : Vector<'t, 'N>, f : 't -> 'u) : Vector<'u, 'N> =
+    static member inline Map(v : Vector<_, _>, f) =
         v.MapArray(map f)
-
 
     static member inline (<*>) (f : Vector<'t -> 'u, 'N>, v : Vector<'t, 'N>) : Vector<'u, 'N> =
         let a =
-            Array.zip f.array v.array
+            Array.zip f.Array v.Array
             |>> fun (f, x) -> f x
-        Vec(a, v.n)
+        Vec(a, v.Peano)
 
-    member inline v.Item (i : ^i) : 'a =
-        eval ty<Sub<Sub< ^n, ^i >, N1>> |> ignore
-        let index = (^i : (static member toInt : ^i -> int) i)
-        v.array.[index]
+    static member inline Return (k : 't, _ : N1) = VectorImpl.Init (k)
+    static member inline Return (k : 't, _ : N2) = VectorImpl.Init (k, k)
+    static member inline Return (k : 't, _ : N3) = VectorImpl.Init (k, k, k)
+    static member inline Return (k : 't, _ : N4) = VectorImpl.Init (k, k, k, k)
+
+    static member inline Join (v : Vector<Vector<'t, 'N>, 'M>) = ()
     
 
-type VectorImpl = VectorImpl with
+and VectorImpl = VectorImpl with
     static member inline Init () =
         Vec( [||], Peano.n0 )
 
@@ -125,12 +154,7 @@ type VectorImpl = VectorImpl with
     static member inline Init (x, y, z, w) =
         Vec( [|x; y; z; w|], Peano.n4 )
 
-    static member inline Unsafe(arr, n) = Vec(arr, n)
-
-    static member inline Return (k : 't, _ : N1) = VectorImpl.Init (k)
-    static member inline Return (k : 't, _ : N2) = VectorImpl.Init (k, k)
-    static member inline Return (k : 't, _ : N3) = VectorImpl.Init (k, k, k)
-    static member inline Return (k : 't, _ : N4) = VectorImpl.Init (k, k, k, k)
+    // static member inline Unsafe(arr, n) = Vec(arr, n)
 
 type 'a Vector0 = Vector<'a, N0>
 type 'a Vector1 = Vector<'a, N1>
@@ -142,10 +166,6 @@ type 'a Vector4 = Vector<'a, N4>
 module Vector2 =
     let inline init x y = VectorImpl.Init(x, y)
 
-    let inline rev (v : 'a Vector2) =
-        let x, y = v.Item Peano.x, v.Item Peano.y
-        init y x
-
 module Vector3 =
     let inline init x y z = VectorImpl.Init(x, y, z)
 
@@ -154,6 +174,11 @@ module Vector4 =
 
 
 module Vector =
+    type ^N GE1 = ^N S
+    type ^N GE2 = ^N GE1 S
+    type ^N GE3 = ^N GE2 S
+    type ^N GE4 = ^N GE3 S
+
     let inline x (v : Vector<_, 'N GE1>) =
         v.Item Peano.x
 
@@ -201,6 +226,7 @@ module Vector =
     let inline zw v = (wz v).MapArray(rev)
     
     open FSharpPlus.Math.Applicative
+
     let inline dot (a : Vector<'a, 'n>) (b : Vector<'a, 'n>) =
         a .*. b
         |> fold ( + ) zero
@@ -213,18 +239,28 @@ module Vector =
 
     let inline normalize (v : Vector<'a, 'n>) : Vector<'a, 'n> =
         v ./ length v
+
+    let inline append (x: Vector<'t, ^N>) (y: Vector<'t, ^M>) : Vector<'t, _> =
+        let a = Array.append x.Array y.Array
+        let n = (eval ty<Add< ^M, ^N >>).Value
+        Vec(a, n)
         
 
 module private Test =
-    let v0 : int Vector0 = VectorImpl.Init()
+    let _v0 : int Vector0 = VectorImpl.Init()
 
-    let v1 = VectorImpl.Init 1
-    let v1_x = v1 |> Vector.x
-    let v1_0 = v1.Item Peano.x
+    let _v1 = VectorImpl.Init 1
+    let _v1_x = _v1 |> Vector.x
+    let _v1_0 = _v1.Item Peano.x
     // let v1_1 = Vector1<_>.Item(v1, Peano.y)
 
-    let v2 = Vector2.init 1 2
-    let v2_x = v2.Item Peano.x
-    let v2_y = v2.Item Peano.y
+    let _v2 = Vector2.init 1 2
+    let _v2_x = _v2.Item Peano.x
+    let _v2_y = _v2.Item Peano.y
 
-    let v2f = float32 <!> v2
+    let _v2f = float32 <!> _v2
+
+    let _v3 = Vector.append _v1 _v2
+    let _v3' = Vector.append _v2 _v1
+    let _v5 = Vector.append _v3 _v2
+
