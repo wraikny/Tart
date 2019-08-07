9f9cb6dbe1f045fe0febec6a15d3b3febcbc1f5d
wraikny <writtenknown@gmail.com>
2019-08-08 00:27:50 +0900

    random as monad

--- src/Tart.Core/Libraries/Random.fs
+++ src/Tart.Core/Libraries/Random.fs
@@ -1,33 +1,23 @@
-﻿namespace wraikny.Tart.Core.Libraries
+namespace wraikny.Tart.Core.Libraries
 open wraikny.Tart.Core
-
+open FSharpPlus
 
 module Random =
     type 'a Generator = internal | Generator of (System.Random -> 'a)
     with
         member inline internal g.F = g |> function | Generator(f) -> f
 
+        static member inline Return(x : 'a) = Generator(fun _ -> x)
+        static member inline (>>=) (x : 'a Generator, f : 'a -> 'b Generator) =
+            Generator(fun rand ->
+                ((x.F rand) |> f).F rand
+            )
+
     // let inline private getFunc (g : _ Generator) = g.F
     
     [<CompiledName "Bind">]
-    let bind(f : 'a -> 'b Generator) (g : 'a Generator) : 'b Generator =
-        Generator(fun rand ->
-            ((g.F rand) |> f).F rand
-        )
-
-    type RandomBuilder() =
-        let mreturn x = Generator(fun _ -> x)
-        member __.Bind(x, k) = bind k x
-        member __.YieldFrom(x) = x
-        member __.ReturnFrom(x) = x
-        member __.Yield(x) = mreturn x
-        member __.Return(x) = mreturn x
-        member __.Delay(f) = f()
-
-        member __.For(inp, f) =
-            seq {for a in inp -> f a}
-
-    let random = RandomBuilder()
+    let inline bind(f : 'a -> 'b Generator) (x : 'a Generator) : 'b Generator =
+        x >>= f
 
     [<CompiledName "Bool">]
     let bool : bool Generator =
@@ -37,38 +27,35 @@ module Random =
 
     
     [<CompiledName "Int">]
-    let int (minValue : int) (maxValue : int) : int Generator =
+    let inline int (minValue : int) (maxValue : int) : int Generator =
         Generator(fun rand ->
             rand.Next(minValue, maxValue)
         )
 
 
     [<CompiledName "Float">]
-    let float (minValue : float) (maxValue : float) : float Generator =
+    let inline float (minValue : float) (maxValue : float) : float Generator =
         Generator(fun rand ->
             minValue + rand.NextDouble() * (maxValue - minValue)
         )
 
 
     [<CompiledName "List">]
-    let list (length : int) (generator : 'a Generator) : ('a list) Generator =
+    let inline list (length : int) (generator : 'a Generator) : ('a list) Generator =
         Generator(fun rand ->
-            [ for _ in 0..length-1 -> generator.F rand ]
+            [ for _ in 1..length -> generator.F rand ]
         )
 
 
 
     [<CompiledName "Map">]
-    let inline map (f : 'a -> 'b) (generator : 'a Generator) : 'b Generator =
-        random {
-            let! x1 = generator
-            return (f x1)
-        }
+    let inline map (f : 'a -> 'b) (x : 'a Generator) : 'b Generator =
+        map f x
 
 
     [<CompiledName "Map2">]
     let inline map2 (f : 'a -> 'b -> 'c) (g1 : 'a Generator) (g2 : 'b Generator) : 'c Generator =
-        random {
+        monad {
             let! x1 = g1
             let! x2 = g2
             return (f x1 x2)
