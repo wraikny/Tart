0dcfb05cc0400dcdd8116720289c62f3b2685b21
wraikny <writtenknown@gmail.com>
2019-08-10 02:52:50 +0900

    random

--- src/Tart.Core/Libraries/Random.fs
+++ src/Tart.Core/Libraries/Random.fs
@@ -8,17 +8,20 @@ module Random =
         member inline internal g.F = g |> function | Generator(f) -> f
 
         static member Return(x : 'a) = Generator(fun _ -> x)
-        static member (>>=) (x : 'a Generator, f : 'a -> 'b Generator) =
+        static member (>>=) (x : 'a Generator, f : 'a -> 'b Generator) : 'b Generator =
             Generator(fun rand ->
                 ((x.F rand) |> f).F rand
             )
+
+        static member Map(x : 'a Generator, f : 'a -> 'b) =
+            Generator(fun rand -> x.F rand |> f)
 
 
     // let inline private getFunc (g : _ Generator) = g.F
     
     [<CompiledName "Bind">]
     let inline bind(f : 'a -> 'b Generator) (x : 'a Generator) : 'b Generator =
-        x >>= f
+        Generator.(>>=)(x, f)
 
     [<CompiledName "Bool">]
     let bool : bool Generator =
@@ -42,16 +45,35 @@ module Random =
 
 
     [<CompiledName "List">]
-    let list (length : int) (generator : 'a Generator) : ('a list) Generator =
+    let list (length : int) (generator : 'a Generator) : 'a list Generator =
         Generator(fun rand ->
             [ for _ in 1..length -> generator.F rand ]
         )
 
+    [<CompiledName "Until">]
+    let until f (generator : 'a Generator) : 'a Generator =
+        monad {
+            let rec loop xs = monad {
+                if f xs then
+                    return xs
+                else
+                    let! nx = generator
+                    return! loop nx
+            }
+            let! x = generator
+            return! loop x
+        }
+
+
+    [<CompiledName "DistinctList">]
+    let distinctList (length : int) (generator : 'a Generator) : 'a list Generator =
+        list length generator
+        |> until (fun xs -> xs |> Seq.distinct |> Seq.length = length)
 
 
     [<CompiledName "Map">]
     let inline map (f : 'a -> 'b) (x : 'a Generator) : 'b Generator =
-        map f x
+        Generator.Map(x, f)
 
 
     [<CompiledName "Map2">]
